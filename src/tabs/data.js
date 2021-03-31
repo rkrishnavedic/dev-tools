ed2048477676f9380aeaf95f3cb420b24914339f
Rahul Krishna <57654450+rkrishnavedic@users.noreply.github.com>
2021-03-31 21:29:24 +0530

    update data

---
+++ src/tabs/data.js
@@ -0,0 +1,43 @@
+export const tableData = [
+    {
+        name:"CF Profile Check",
+        status:1,
+        completiontime:"March 21"
+    },
+    {
+        name:"CF Contest Submission Check",
+        status:1,
+        completiontime:"March 21"
+    },
+    {
+        name:"Code Editor",
+        status:1,
+        completiontime:"March 21"
+    },
+    {
+        name:"CF Contest Standings Check",
+        status:0,
+        completiontime:"---"
+    }
+]
+
+export const appreciationsData =[
+    {
+        quote:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam obcaecati laudantium recusandae, debitis eum voluptatem ad, illo voluptatibus temporibus odio provident.",
+        name:"Adarsh Balika",
+        handle:"@a.balika",
+        avatarURL:"https://randomuser.me/api/portraits/women/11.jpg"
+    },
+    {
+        quote:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam obcaecati laudantium recusandae, debitis eum voluptatem ad, illo voluptatibus temporibus odio provident.",
+        name:"Adarsh Balika",
+        handle:"@a.balika",
+        avatarURL:"https://randomuser.me/api/portraits/women/11.jpg"
+    },
+    {
+        quote:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam obcaecati laudantium recusandae, debitis eum voluptatem ad, illo voluptatibus temporibus odio provident.",
+        name:"Adarsh Balika",
+        handle:"@a.balika",
+        avatarURL:"https://randomuser.me/api/portraits/women/11.jpg"
+    },
+]
--- src/tabs/home.js
+++ src/tabs/home.js
@@ -1,28 +1,6 @@
 import React from 'react';
 import '../App.css'
-
-const tableData = [
-    {
-        name:"CF Profile Check",
-        status:1,
-        completiontime:"March 21"
-    },
-    {
-        name:"CF Contest Submission Check",
-        status:1,
-        completiontime:"March 21"
-    },
-    {
-        name:"Code Editor",
-        status:1,
-        completiontime:"March 21"
-    },
-    {
-        name:"CF Contest Standings Check",
-        status:0,
-        completiontime:"---"
-    }
-]
+import {appreciationsData, tableData } from './data.js';
 
 function Home(){
 
@@ -49,14 +27,40 @@ function Home(){
         <div style={{marginTop:"5rem"}} className="font-sans">
             <small className="m-2 text-gray-400"> /home </small>
                 <hr/>
-            <div className="m-4 p-3 text-center outline-black shadow-md">
+            <div className="m-4 p-3 text-center shadow-md">
                 <div className="text-2xl">
-            Welcome to my new learning project!
-                </div>
-                <div className="text-justify text-gray-500">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            Welcome to my new learning series!
                 </div>
             </div>
+
+            <nav id="appreciations" style={{justifyContent:'center', alignItems:"center"}} className=" flex gap-5 items-center m-auto" >
+
+                {appreciationsData.map( (value, index)=>{
+                    return(
+                    <div class="flex-1 items-center justify-center px-5 py-5">
+                        <div class="rounded-lg bg-white shadow-lg px-5 pt-5 pb-10 text-gray-800" style={{maxWidth: "20rem"}}>
+                            <div class="pt-1 pb-5">
+                                <div class="overflow-hidden rounded-full w-20 h-20 -mt-16 mx-auto shadow-lg">
+                                    <img src={value.avatarURL} alt="avatar"/>
+                                </div>
+                            </div>
+                            <div class="mb-10">
+                                <div class="text-3xl text-indigo-500 text-left leading-tight h-3">“</div>
+                                <p class="text-sm text-gray-600 text-center px-5">{value.quote}</p>
+                                <div class="text-3xl text-indigo-500 text-right leading-tight h-3 -mt-3">”</div>
+                            </div>
+                            <div >
+                                <p class="text-md text-indigo-500 font-bold text-center">{value.name}</p>
+                                <p class="text-xs text-gray-500 text-center">{value.handle}</p>
+                            </div>
+                        </div>
+                    </div>
+                    )
+                }
+                )}
+            </nav>
+
+
             <table className="m-auto text-center table-auto shadow-md">
                 <thead className="border px-4 py-2">
                     <tr>
@@ -77,6 +81,12 @@ function Home(){
                     })}
                 </tbody>
             </table>
+            <div className="text-center float-right text-gray-500">
+                <span>
+                Learning something new always make you feel better. Do you remember how you started to walk and talk as you grown up? In the same strategy let us learn and grow together.
+                </span>
+            </div>
+           
         </div>
     )
 }
