a9814ab3a88c41056745ec47fc1fdcfc8cedfe0d
Rahul Krishna <57654450+rkrishnavedic@users.noreply.github.com>
2021-04-04 16:04:01 +0530

    fix:

--- src/tabs/blogs.js
+++ src/tabs/blogs.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, {useState } from 'react';
 import '../App.css';
 
 const blogsAPIURL = "https://blogapi89.herokuapp.com/api/v1/faqs/";
@@ -7,21 +7,38 @@ const blogsAPIURL = "https://blogapi89.herokuapp.com/api/v1/faqs/";
 function Blogs(){
 
     const [blogsData, setblogsData] = useState([]);
+    // const [laodingStatus, setLoadingStatus] = useState(false);
 
-    useEffect(()=>{
-        const fetchData = async ()=>{
+    // function Loading(){
+    //     if(laodingStatus){
+    //         return(
+    //         <div id="loading" class="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center">
+    //         <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+    //         </div>
+    //         )
+    //     }
+    //     return (<div></div>)
+    // }
+    
+    const fetchData = async ()=>{
+            
             await axios.get(blogsAPIURL)
                     .then((res)=>{
                         //console.log(res.data);
                         setblogsData(res.data);
-                    }).catch(err=>console.log(err));
+                        
+                    }).catch((err)=>{
+                        
+                        console.log(err);
+                    });
+            
                 }
-        fetchData();
-    },blogsData)
+    fetchData();
+    
 
     return (
         <div className="w-2 m-2 font-sans">
-
+            
             <div className="grid">
                 {blogsData.map((value,index)=>{
                     return (
